2cbbf1befaa3193f7f1f6e7a2cfb61c0f766aad4
Hikaru <ckg24043@st.ckg.ac.jp>
2025-12-16 09:23:09 +0900

    future ページネーション機能＋ダミー投稿機能実装
                modified:   .gitignore  static/uploadsに変更
                modified:   app/routers/posts.py    ダミー投稿機能、ページネーション対応
                modified:   app/routers/threads.py    ダミー投稿機能、ページネーション対応
                new file:   app/static/css/style.css    外部スタイルシート適用
                modified:   app/templates/base.html     外部スタイルシート適用
                new file:   app/templates/common/pagination.html    共通化ページネーション
                modified:   app/templates/index.html    ページネーション対応
                modified:   app/templates/thread_detail.html    ページネーション対応
                modified:   requirements.txt    ダミー投稿用Faker追加

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers import threads
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI(title="BBS API - Step1")
 
@@ -8,6 +9,12 @@ app = FastAPI(title="BBS API - Step1")
 app.include_router(threads.router)
 
 from app.routers import posts  # ← 追加
+
+app.mount(
+    "/static", # ブラウザがアクセスするURLパス (例: http://.../static/css/style.css)
+    StaticFiles(directory="app/static"), # ← Dockerコンテナ内でのディレクトリ構造
+    name="static"
+)
 
 app.include_router(threads.router)
 app.include_router(posts.router)
